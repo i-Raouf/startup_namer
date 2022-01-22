e7f70065f360bab95b5ae59b3481862040edae85
Raouf <iraouf86@gmail.com>
2022-01-22 14:34:32 +0100

    clear up unused variable

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
